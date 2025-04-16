b5317497c92b76753fe89991d494196dd996e401
Weixing Zhang <weixingzhang@MacBook-Air-de-WZ.local>
2025-04-15 22:21:21 -0700

    Understood the main struct: FastPFOR

--- tests/custom_tests.rs
+++ tests/custom_tests.rs
@@ -5,12 +5,12 @@ use fastpfor::rust::{FastPFOR, Integer, DEFAULT_PAGE_SIZE, BLOCK_SIZE_128};
 
 #[test]
 fn simple_fastpfor_test() {
-    let input: Vec<u32> = (0..BLOCK_SIZE_128).collect(); // works best with 128 values
-    let mut output: Vec<u32> = vec![0; input.len() * 4]; // make sure it's big enough
+    let input: Vec<u32> = (0..BLOCK_SIZE_128).collect();
+    let mut output: Vec<u32> = vec![0; input.len()]; 
     let mut decoded: Vec<u32> = vec![0; input.len()];
     let mut input_offset = Cursor::new(0u32);
     let mut output_offset = Cursor::new(0u32);
-
+    
     let mut codec = FastPFOR::new(DEFAULT_PAGE_SIZE, BLOCK_SIZE_128);
 
     codec
@@ -23,18 +23,30 @@ fn simple_fastpfor_test() {
         )
         .expect("compression failed");
 
-    let len = output_offset.position() as u32;
-    output_offset.set_position(0);
+    let compressed_len = output_offset.position() as usize;
+    input_offset.set_position(0);
 
     codec
         .uncompress(
             &output,
-            len,
-            &mut output_offset,
+            compressed_len as u32,
+            &mut input_offset,
             &mut decoded,
             &mut Cursor::new(0u32),
         )
         .expect("decompression failed");
+    
+    println!("Input: {:?} with bytes", &input); 
+    println!("Encoded: {:?}", &output);
+    println!("Decoded: {:?}", &decoded);
+
+    let original_size = input.len() * size_of::<u32>();
+    let encoded_size = compressed_len * size_of::<u32>();
+    let decoded_size = decoded.len() * size_of::<u32>();
+
+    println!("Original size: {} bytes", original_size);
+    println!("Encoded size: {} bytes", encoded_size);
+    println!("Decoded size: {} bytes", decoded_size);
 
     assert_eq!(input, decoded, "Input and decompressed data do not match");
 }
